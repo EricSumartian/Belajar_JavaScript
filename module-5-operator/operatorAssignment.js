7bf34ccd703ca716fa17b9f8dfdcb8eb87329f1b
EricSumartian <ericsumartian06@gmail.com>
2025-06-14 07:40:43 +0700

    operator assignment

---
+++ module-5-operator/operatorAssignment.js
@@ -0,0 +1,29 @@
+/** Operator Assignment */
+// Assignment Dasar 
+let nama = "Eric Sumartian"
+console.log(nama)
+
+// Penjumlahan dan Assignment +=
+// let x = 7 
+// x = x + 3 //x += 3
+// console.log(x) 
+
+// Pegurangan dan Assignment -=
+// let x = 7 
+// x -= 3 
+// console.log(x) 
+
+// Perkalian dan Assignment *=
+// let x = 7
+// x *= 3
+// console.log(x)
+
+// Pembagian dan Assignment /=
+// let x = 12
+// x /= 3
+// console.log(x)
+
+// Modulus dan Assignment %=
+let x = 13
+x %= 3
+console.log(x)
