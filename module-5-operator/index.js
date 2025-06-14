fa1426932188683c3223a32d4cfa964fc2d5d315
EricSumartian <ericsumartian06@gmail.com>
2025-06-14 07:11:36 +0700

    operator aritmatika

---
+++ module-5-operator/index.js
@@ -0,0 +1,47 @@
+/* Operator Aritmatika */
+
+// Penjumlahan +
+let x = 5
+let y = 10
+let sum = x + y
+console.log(sum, typeof sum)
+
+// Pengurangan -
+let a = 7
+let b = 3
+let deff = a - b
+console.log(deff, typeof deff)
+
+// Perkalian *
+let p = 5
+let q = 6
+let result = p * q
+console.log(result, typeof result)
+
+// Pembagian /
+let m = 100
+let n = 4
+let division = m / n
+console.log(division, typeof division)
+
+// modulus %
+let c = 15
+let d = 4
+let modulus = c % d // modulus adalah sisa bagi -> 15 / 4 = 3 sisa 3
+console.log(modulus, typeof modulus)
+
+// Eksponen **
+let base = 2
+let exponent = 3
+let exponentiation = base ** exponent // 2 pangkat 3
+console.log(exponentiation, typeof exponentiation)
+
+// Increment ++
+let increment = 10
+let incrementedValue = increment + 1 // atau increment++
+console.log(incrementedValue, typeof incrementedValue)
+
+// Decrement --
+let decrement = 10
+let decrementedValue = decrement - 1 // atau decrement--
+console.log(decrementedValue, typeof decrementedValue)
